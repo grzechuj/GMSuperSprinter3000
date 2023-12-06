9758844ea52f325d653bc5cb8680e6ce0842e474
Grzegorz Matusinski <greg@igamemedia.com>
2023-12-06 15:59:02 +0000

    bootstrap project

---
+++ src/main/java/pl/codecool/gmsupersprinter3000/GmSuperSprinter3000Application.java
@@ -0,0 +1,13 @@
+package pl.codecool.gmsupersprinter3000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmSuperSprinter3000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmSuperSprinter3000Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/codecool/gmsupersprinter3000/GmSuperSprinter3000ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.codecool.gmsupersprinter3000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmSuperSprinter3000ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
